5869d1f972e5428a8a68d24493d8260d31bcc372
Shiv <shivverma1996@gmail.com>
2015-04-04 23:22:15 -0500

    edited the gitignore

--- build/CMakeFiles/yay.dir/DependInfo.cmake
+++ build/CMakeFiles/yay.dir/DependInfo.cmake
@@ -4,10 +4,19 @@ SET(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 SET(CMAKE_DEPENDS_CHECK_CXX
-  "/home/utsav/projects/src/yay.cpp" "/home/utsav/projects/src/build/CMakeFiles/yay.dir/yay.cpp.o"
+  "/home/shiv/Dev/battle-bots/yay.cpp" "/home/shiv/Dev/battle-bots/build/CMakeFiles/yay.dir/yay.cpp.o"
   )
 SET(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Targets to which this target links.
 SET(CMAKE_TARGET_LINKED_INFO_FILES
   )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/include/python2.7"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
