2ebc27b1fc694d66135ba1af755c8026e3273728
Shiv <shivverma1996@gmail.com>
2015-04-09 02:36:05 -0500

    using unittests now

--- tests/link_tester.py
+++
@@ -1,8 +0,0 @@
-from config_tests import TestException, ok
-try:
-	import libyay
-except ImportError as e:
-	addendum = "Did you remember to run ./configure? \nOr you might have not added build to PYTHONPATH"
-	raise TestException(e,  addendum) 
-
-print(ok("link test set passed"))
--- tests/test_libtbtg.py
+++ tests/test_libtbtg.py
@@ -1,17 +1,18 @@
-from  config_tests import TestException, ok, warning
-try:
-	import libTurnBased
+from  config_tests import TestException, ok, warning as w
+import unittest
 
-	gc = libTurnBased.TB_core(2)
-	p1, p2 = gc.get_player_list()
+class libTurnBasedTest(unittest.TestCase):
+	def test(self):
+		import libTurnBased
 
-	while (gc.get_winner() == -1):
-		p1.do_turn(0)
-		p2.do_turn(1)
-except Exception as e:
-	raise TestException(e, "")
+		gc = libTurnBased.TB_core(2)
+		p1, p2 = gc.get_player_list()
 
-if (gc.get_winner() != 1):
-	print warning("The winner value is incorrect. Check your game logic\n")
-else:
-	print ok("Turn By Turn Game library tests passed.\n") 
+		while (gc.get_winner() == -1):
+			p1.do_turn(0)
+			p2.do_turn(1)	
+		
+		self.failUnlessEqual(gc.get_winner(), 1,w("The winner value is incorrect. Check your game logic\n"))
+
+if __name__ == '__main__':
+    unittest.main()
