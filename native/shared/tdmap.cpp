6d6bbcf1c0c72da0f0b1daed8eb6c6be3c6d8e79
Shiv Verma <shivverma1996@gmail.com>
2015-04-17 16:17:21 -0500

    some logic fixes in tdmap

--- native/shared/tdmap.cpp
+++ native/shared/tdmap.cpp
@@ -1,8 +1,14 @@
+/*
+   Just a constructor.
+*/
 TDMap::TDMap(int width, int height, std::vector<Path *> paths)
 	:dimensions(Coordinate(width,height)),paths(paths)
 {
 }
 
+/*
+   Just another constructor.
+*/
 TDMap::TDMap(Coordinate dimensions, std::vector<Path *> paths)
 	:dimensions(dimensions)
 {
@@ -11,10 +17,8 @@ TDMap::TDMap(Coordinate dimensions, std::vector<Path *> paths)
 }
 
 /*
-boost does range checking, so we dont need to do it
-we can think about it later.
+   returns all the sprites at coordinate c.
 */
-
 std::vector<Sprite *> TDMap::get_sprites_at(Coordinate c)
 {
 	std::vector<Sprite *> to_return(0);
@@ -28,6 +32,9 @@ std::vector<Sprite *> TDMap::get_sprites_at(Coordinate c)
 	return to_return;
 }
 
+/*
+   returns the Tower at coordinate c.
+*/
 Tower * TDMap::get_tower_at(Coordinate c)
 {
 	BOOST_FOREACH(Tower * t, this->towers)
@@ -40,29 +47,51 @@ Tower * TDMap::get_tower_at(Coordinate c)
 	return nullptr;	
 }
 
+/*
+   returns the sprites at coordinate (row,column).
+*/
 std::vector<Sprite *> TDMap::get_sprites_at(int row, int column)
 {
 	return get_sprites_at(Coordinate(row,column));
 }
 
+/*
+   returns the Tower at coordinate (row,column).
+*/
 Tower * TDMap::get_tower_at(int row, int column)
 {
 	return get_tower_at(Coordinate(row,column));
 }
 
+/*
+   Adds a tower.
+   Returns true if tower successfully added.
+*/
 bool TDMap::add_tower(Tower * tower)
 {
-	if (get_tower_at(tower->get_coordinate()) != nullptr)
+    Coordinate c = tower->get_coordinate();
+    Coordinate d = this->dimensions;
+    if ( ! (c.x < d.x && c.y < d.y ))
+        return false;
+
+	if (get_tower_at(c) != nullptr)
 		return false;
 
 	this->towers.push_back(tower);
 	return true;
 }
 
+/*
+   Adds a tower.
+   Returns true if sprite successfully added.
+*/
 bool TDMap::add_sprite(Sprite * sprite)
 {
-	if (!get_sprites_at(sprite->get_coordinate()).empty())
-		return false;
+
+    Coordinate c = sprite->get_coordinate();
+    Coordinate d = this->dimensions;
+    if ( ! (c.x < d.x && c.y < d.y ))
+        return false;
 
 	this->sprites.push_back(sprite);
 	return true;
